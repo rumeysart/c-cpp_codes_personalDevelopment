edd80ae1fbad0bc7d58d0c7bd46bad8864970084
Rümeysa TATAR <111748034+rumeysart@users.noreply.github.com>
2024-02-04 16:27:43 +0300

    Create linked_list_3.c

---
+++ cpp/data_structures2/linked_list_3.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+//LESSON_3
+//Insertion and Sequential Inserter Function in Linked List
+typedef struct n{
+	int x;
+	struct n *next;
+}  node;
+
+void show(node *r)
+{
+	while (r != NULL)
+	{
+		printf("%d ", r -> x);
+		r = r ->next;
+	}
+}
+
+void add(node *r, int x)
+{
+	while (r -> next)
+	{
+		r = r ->next;
+	}
+	r ->next = (node *)malloc(sizeof(node));
+	r -> next ->x = x;
+	r ->next ->next = NULL;
+}
+
+node *add_sort(node *r, int x)
+{
+	if (r == NULL) //linked_list'imiz boşsa
+	{
+		r = (node *)malloc(sizeof(node));
+		r ->next = NULL;
+		r -> x = x;
+		return (r);
+	}
+	if (r -> x > x) //ilk elemandan küçük durumu
+	{				//root değişiyor
+		node *temp = (node *)malloc(sizeof(node));
+		temp -> x = x;
+		temp -> next = r;
+		return (temp);
+	}
+	node *iter = r;
+	while ( iter-> next != NULL && iter -> next -> x < x)
+	{
+		iter = iter ->next;
+	}
+	node *temp = (node *)malloc(sizeof(node));
+	temp ->next = iter ->next;
+	iter ->next = temp;
+	temp ->x = x;
+	return (r);
+}
+
+int main()
+{
+	node *root;
+	root = NULL;
+	root = add_sort(root, 400);
+	root = add_sort(root, 40);
+	root = add_sort(root, 4);
+	root = add_sort(root, 450);
+	root = add_sort(root, 50);
+	show(root);
+}
